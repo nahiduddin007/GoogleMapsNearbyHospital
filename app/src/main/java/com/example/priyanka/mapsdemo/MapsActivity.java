5036d47a331976e12f5abe2c1ff2bd4c6e4a553d
RafatMeraz <razhossain201@gmail.com>
2019-04-19 12:41:53 +0600

    1st commit

--- app/src/main/java/com/example/priyanka/mapsdemo/MapsActivity.java
+++ app/src/main/java/com/example/priyanka/mapsdemo/MapsActivity.java
@@ -190,27 +190,7 @@ LocationListener{
                 break;
 
 
-            case R.id.B_schools:
-                mMap.clear();
-                String school = "school";
-                url = getUrl(latitude, longitude, school);
-                dataTransfer[0] = mMap;
-                dataTransfer[1] = url;
 
-                getNearbyPlacesData.execute(dataTransfer);
-                Toast.makeText(MapsActivity.this, "Showing Nearby Schools", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.B_restaurants:
-                mMap.clear();
-                String resturant = "restuarant";
-                url = getUrl(latitude, longitude, resturant);
-                dataTransfer[0] = mMap;
-                dataTransfer[1] = url;
-
-                getNearbyPlacesData.execute(dataTransfer);
-                Toast.makeText(MapsActivity.this, "Showing Nearby Restaurants", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.B_to:
         }
     }
 
